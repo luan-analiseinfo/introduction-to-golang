3b595c325c9b8805abbcf8e7cc4e0f8164d0d203
Luan L <lrodrigues.solks@digio.com.br>
2023-06-13 11:49:02 -0300

    section2: data types string :class7

---
+++ section2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	a := "My name"
+	b := "Luan"
+
+	fmt.Println(strings.Split(a, " "), a+b)
+}
+
+/*func main() {
+	a := 1
+	b := 2
+
+	fmt.Println(a + b)
+}
+*/
